2cfce869cfe9633a0c2405d96916a82f087c9bd5
Adrian Karanja <karanja981@gmail.com>
2020-11-18 13:17:06 -0500

    React Setup

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcomt to MyShop</h1>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
