6e25d1a7d34664de7db239018aff62ec7bfba869
Adrian Karanja <karanja981@gmail.com>
2020-12-02 12:09:52 -0500

    Checkout process setup part1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
 
 const App = () => {
 	return (
@@ -16,8 +19,11 @@ const App = () => {
 
 			<main className='py-3'>
 				<Container>
+					<Route path='/shipping' component={ShippingScreen} />
+					<Route path='/payment' component={PaymentScreen} />
 					<Route path='/login' component={LoginScreen} />
 					<Route path='/register' component={RegisterScreen} />
+					<Route path='/profile' component={ProfileScreen} />
 					<Route path='/' component={HomeScreen} exact />
 					<Route path='/product/:id' component={ProductScreen} />
 					<Route path='/cart/:id?' component={CartScreen} />
