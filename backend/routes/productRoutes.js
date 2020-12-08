1ca799ddc613e0b92cbcb157792c71fec73a43c3
Adrian Karanja <karanja981@gmail.com>
2020-12-08 00:01:04 -0500

    Product delete admin only set up

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import { protect, isAdmin } from '../middleware/authMiddleware.js'
+
 const router = express.Router()
 import {
 	getProducts,
 	getProductById,
+	deleteProduct,
 } from '../controllers/productController.js'
 
 router.route('/').get(getProducts)
-router.route('/:id').get(getProductById)
+router
+	.route('/:id')
+	.get(getProductById)
+	.delete(protect, isAdmin, deleteProduct)
 
 export default router
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts } from '../actions/productActions'
+import { listProducts, deleteProduct } from '../actions/productActions'
 
 const ProductListScreen = ({ match, history }) => {
 	const dispatch = useDispatch()
@@ -14,17 +14,24 @@ const ProductListScreen = ({ match, history }) => {
 	const userLogin = useSelector((state) => state.userLogin)
 	const { userInfo } = userLogin
 
+	const productDelete = useSelector((state) => state.productDelete)
+	const {
+		loading: loadingDelete,
+		error: errorDelete,
+		success: successDelete,
+	} = productDelete
+
 	useEffect(() => {
 		if (userInfo && userInfo.isAdmin) {
 			dispatch(listProducts())
 		} else {
 			history.push('/login')
 		}
-	}, [dispatch, history, userInfo])
+	}, [dispatch, history, userInfo, successDelete])
 
 	const deleteHandler = (id) => {
 		if (window.confirm('Are you sure')) {
-			//DELETE PRODUCTS
+			dispatch(deleteProduct(id))
 		}
 	}
 
@@ -43,7 +50,8 @@ const ProductListScreen = ({ match, history }) => {
 					</Button>
 				</Col>
 			</Row>
-
+			{loadingDelete && <Loader />}
+			{errorDelete && <Message variant='default'>{errorDelete}</Message>}
 			{loading ? (
 				<Loader />
 			) : error ? (
@@ -81,7 +89,7 @@ const ProductListScreen = ({ match, history }) => {
 											deleteHandler(product._id)
 										}}
 									>
-										<i className='fas fa-delete'></i>
+										<i className='fas fa-trash'></i>
 									</Button>
 								</td>
 							</tr>
